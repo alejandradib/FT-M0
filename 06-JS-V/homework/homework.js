1fb198c529fec1e71ad6b50f69d7b0b0f0c0ea0b
alejandradib <alejandradib@gmail.com>
2021-08-12 10:58:55 -0300

    finish homework JSV

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,12 +8,43 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  function Usuario(opciones){
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.password = opciones.password;
+  }
+  Usuario.prototype.saludar = function() {
+    return 'Hola, mi nombre es ' + this.nombre;
+  };
+
+  /* PUEDE SER TMB
+
+  class Usuario {
+    constructor(opciones){
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.password = opciones.password;
+    }
+    saludar(){
+      return 'Hola, mi nombre es ' + this.nombre;
+      }
+    }
+
+    */
+  return Usuario;
+
 }
 
 function agregarMetodoPrototype(Constructor) {
-  // Agrega un método al Constructor del `prototype`
+  // Agrega un método al Constructor del `prototype` // es al prototype del constructor
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function() {
+    return "Hello World!";
+  }
+
 }
 
 function agregarStringInvertida() {
@@ -22,6 +53,26 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+   String.prototype.reverse = function(){
+    return this.split("").reverse().join("")
+    /*Cuando digo this hago referencia a la palabra que esta en la instancia de string, en este caso toni.
+    A Toni le hago un Split, y me separa cada uno de los caracteres uno a uno y los pone adentro de mi array
+    El reverse a esos caracteres que split separó los puso al reves, pero separados
+    Y el join me va a juntar todos los elementos del array pero separados por lo que yo le pase, es decir un string vacio ""
+    */
+
+    /*OTRA FORMA DE HACERLO
+    String.prototype.reverse = function(){
+     var arr = this.split("")
+     var arr2 = []
+     for (var i= arr.length-1; i >= 0; i--){
+       arr2.push(arr[i])
+     }
+     return arr2.join("")
+    }
+
+    */
+   }
 }
 
 // ---------------------------------------------------------------------------//
@@ -36,9 +87,20 @@ function agregarStringInvertida() {
     //  }
 
   class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
+    constructor(/*Escribir los argumentos que recibe el constructor*/nombre, apellido, edad, domicilio) {
       // Crea el constructor:
-
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio;
+    }
+    detalle(){
+      return{
+        nombre: this.nombre,
+        apellido: this.apellido,
+        edad: this.edad,
+        domicilio: this.domicilio,
+      }
     }
 }
 
@@ -46,11 +108,15 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  return new Persona (nombre, apellido, edad, dir)
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+  Persona.prototype.datos = function() {
+    return `${this.nombre}, ${this.edad} años`
+  }
 }
   
 
